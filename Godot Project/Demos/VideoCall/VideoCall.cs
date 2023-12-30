0bac2980d44ccad9949f7fba3e8a4ed987a829f9
Lulu <dev@sakul-flee.de>
2023-12-30 23:10:27 +0100

    Almost working

--- Godot Project/Demos/VideoCall/VideoCall.cs
+++ Godot Project/Demos/VideoCall/VideoCall.cs
@@ -1,14 +1,12 @@
+using System;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
-using System.Threading.Tasks;
 using FlashCap;
 using Godot;
-using MathNet.Numerics.LinearAlgebra;
-using MathNet.Numerics.LinearAlgebra.Complex;
 
 public partial class VideoCall : Node
 {
-	private static Matrix<double> YUVtoRGBConversionMatrix = Matrix<double>.Build.DenseOfArray(new double[,] { { 0.2126, 0.7152, 0.0722 }, { -0.09991, -0.33609, 0.436 }, { 0.615, -0.55861, -0.05639 } });
-
 	private MatchMaker matchMaker;
 
 	private ItemList captureDeviceList;
@@ -48,7 +46,7 @@ public partial class VideoCall : Node
 
 	public override void _Ready()
 	{
-		matchMaker.OnMessageString += OnChannelMessageReceived;
+		matchMaker.OnMessageRaw += OnChannelMessageReceivedRaw;
 
 		captureDevices = new CaptureDevices();
 		foreach (var descriptor in captureDevices.EnumerateDescriptors())
@@ -69,17 +67,6 @@ public partial class VideoCall : Node
 				GD.Print($"[VideoCall] Found capture device with no characteristics! ({descriptor})");
 			}
 		}
-
-
-		// // If we are a host, wait for a channel to open and send an initial message
-		// matchMaker.OnChannelOpen += (peerUUID, channel) =>
-		// {
-		// 	if (matchMaker.IsHost)
-		// 	{
-		// 		GD.Print("[PingPong] Channel opened! Sending initial message ...");
-		// 		matchMaker.SendOnChannelString(peerUUID, channel, "Ping!");
-		// 	}
-		// };
 	}
 
 	public override void _Process(double delta)
@@ -116,30 +103,58 @@ public partial class VideoCall : Node
 		GD.Print("[VideoCall] Capture device opened and started!");
 	}
 
-	private async Task OnNewFrame(PixelBufferScope pixelBuffer)
+	private void OnNewFrame(PixelBufferScope pixelBuffer)
 	{
-		// Update label
-
-
 		// Get the bitmap data from the pixel buffer.
 		// This data can be in various formats (such as YUV or RBG) depending on the capture device.
 		// We hope that Godot does supports converting whatever format your webcam is outputting natively.
 		// However, in some cases, you may need to convert the given format to something Godot understands before creating an Image from it.
 		byte[] bitmapData = pixelBuffer.Buffer.ExtractImage();
 
+		// Assign the texture.
+		// ⚠️ The texture must be set inside a Godot thread! ⚠️
+		var image = imageFromBitMap(bitmapData);
+
+		var imageTexture = ImageTexture.CreateFromImage(image);
+		setLocalFrame(imageTexture, pixelBuffer.Buffer.FrameIndex);
+
+		// Send frame
+		sendFrame(image, pixelBuffer.Buffer.FrameIndex);
+	}
+
+	private Image imageFromBitMap(byte[] bitmapData)
+	{
 		// Create an image from the bitmap data
 		var image = new Image();
 		var error = image.LoadBmpFromBuffer(bitmapData);
 		if (error != Error.Ok)
 		{
 			GD.PrintErr("[VideoCall] Failed converting webcam data into something useable ...");
-			return;
+			return null;
 		}
 
-		// Assign the texture.
-		// ⚠️ The texture must be set inside a Godot thread! ⚠️
-		var imageTexture = ImageTexture.CreateFromImage(image);
-		CallDeferred("setLocalFrame", imageTexture, pixelBuffer.Buffer.FrameIndex);
+		// Resize to fit buffer
+		return image;
+	}
+
+	private Image imageFromJPG(byte[] data)
+	{
+		// Create an image from the bitmap data
+		var image = new Image();
+		var error = image.LoadJpgFromBuffer(data);
+		if (error != Error.Ok)
+		{
+			GD.PrintErr("[VideoCall] Failed converting JPG buffer");
+			return null;
+		}
+
+		// Resize to fit buffer
+		return image;
+	}
+
+	private void setLocalFrame(ImageTexture imageTexture, long frameIndex)
+	{
+		CallDeferred("_setLocalFrame", imageTexture, frameIndex);
 	}
 
 	/// <summary>
@@ -150,15 +165,59 @@ public partial class VideoCall : Node
 	/// 
 	/// </summary>
 	/// <param name="frameData"></param>
-	private void setLocalFrame(ImageTexture imageTexture, long frameIndex)
+	private void _setLocalFrame(ImageTexture imageTexture, long frameIndex)
 	{
 		localVideo.Texture = imageTexture;
 
 		localFrameLabel.Text = string.Format(localFrameLabelTemplate, frameIndex);
 	}
 
-	private void OnChannelMessageReceived(string peerUUID, ushort channel, string message)
+	private void setRemoteFrame(ImageTexture imageTexture, long frameIndex)
 	{
+		CallDeferred("_setRemoteFrame", imageTexture, frameIndex);
+	}
 
+	/// <summary>
+	/// ⚠️ Call this function with `CallDeferred` to run on a Godot thread! ⚠️
+	/// Sets the local frame to the image texture
+	/// </summary>
+	/// <param name="frameData"></param> <summary>
+	/// 
+	/// </summary>
+	/// <param name="frameData"></param>
+	private void _setRemoteFrame(ImageTexture imageTexture, long frameIndex)
+	{
+		remoteVideo.Texture = imageTexture;
+
+		remoteFrameLabel.Text = string.Format(remoteFrameLabelTemplate, frameIndex);
+	}
+
+	private void sendFrame(Image image, long frameIndex)
+	{
+		var jpgBuffer = image.SaveJpgToBuffer();
+		var compressed = GZIP.Compress(jpgBuffer);
+
+		var indexToByte = BitConverter.GetBytes(frameIndex);
+
+		var combined = indexToByte.Concat(compressed).ToArray();
+
+		var peer = matchMaker.webRTCConnections.First();
+		GD.Print($"[VideoCall] #{frameIndex} Sending: {combined.Length}b");
+		peer.Value.SendOnChannelRaw(0, combined);
+	}
+
+	private void OnChannelMessageReceivedRaw(string peerUUID, ushort channel, byte[] data)
+	{
+		var frameIndexBuffer = data.Take(8).ToArray();
+		var frameIndex = BitConverter.ToInt64(frameIndexBuffer);
+
+		var compressedImageData = data.Skip(8).Take(data.Length - 8).ToArray();
+		var decompressedImageData = GZIP.Decompress(compressedImageData);
+
+		var image = imageFromJPG(decompressedImageData);
+		var imageTexture = ImageTexture.CreateFromImage(image);
+
+		setRemoteFrame(imageTexture, frameIndex);
+		GD.Print($"[VideoCall] #{frameIndex} Received: {decompressedImageData.Length}b");
 	}
 }
