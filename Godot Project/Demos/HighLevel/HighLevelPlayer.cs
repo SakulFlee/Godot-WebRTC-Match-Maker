d2801792832b2f946cde7d22f548826b7294e881
Lulu <dev@sakul-flee.de>
2024-01-01 04:53:40 +0100

    Working player controller

--- Godot Project/Demos/HighLevel/HighLevelPlayer.cs
+++ Godot Project/Demos/HighLevel/HighLevelPlayer.cs
@@ -2,14 +2,12 @@ using Godot;
 
 public partial class HighLevelPlayer : CharacterBody2D
 {
-	public override void _Process(double delta)
-	{
-		var inputDirection = Input.GetVector("left", "right", "up", "down");
-		Velocity = inputDirection;
-	}
+	[Export]
+	public float SpeedMultiplier = 400.0f;
 
 	public override void _PhysicsProcess(double delta)
 	{
+		Velocity = Input.GetVector("Move Left", "Move Right", "Move Forward", "Move Backward") * SpeedMultiplier;
 		MoveAndSlide();
 	}
 }
