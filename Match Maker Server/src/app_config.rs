2b789382dc80c212bff46958764a5338c439fdc2
Lulu <dev@sakul-flee.de>
2024-01-03 19:50:44 +0100

    Added RPC to config generation

--- Match Maker Server/src/app_config.rs
+++ Match Maker Server/src/app_config.rs
@@ -55,7 +55,7 @@ impl Default for AppConfig {
         slots.insert(String::from("Game"), 2);
         slots.insert(String::from("MultiChannel"), 2);
         slots.insert(String::from("VideoCall"), 2);
-        slots.insert(String::from("HighLevel"), 2);
+        slots.insert(String::from("RPC"), 2);
 
         Self {
             listen_address: String::from("0.0.0.0"),
